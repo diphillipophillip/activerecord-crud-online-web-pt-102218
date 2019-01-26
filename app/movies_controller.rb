a2d95549ed68ac0a9eabc5f53503c6097890c80e
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-01-26 19:36:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,10 +27,7 @@ end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990 })
   
-  Movie.create do |m|
-    m.title = "The Room"
-    m.release_date = 2003
-    m.save
+ 
   end
 end
 
